afacf1d951f85e94d10f58173f9c214bcac19a02
Farai Zihove <farai.zihove@econet.co.zw>
2025-02-19 16:14:21 +0200

    add product table in the schema

--- src/main/resources/db/changelog/schema.sql
+++ src/main/resources/db/changelog/schema.sql
@@ -1,13 +1,30 @@
 -- Create customer table
-CREATE TABLE customer (
-                          id BIGSERIAL PRIMARY KEY,
-                          name VARCHAR(255) NOT NULL
+CREATE TABLE customer
+(
+    id   BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
 
 -- Create order table
-CREATE TABLE "order" (
-                         id BIGSERIAL PRIMARY KEY,
-                         description VARCHAR(255) NOT NULL,
-                         customer_id BIGINT NOT NULL,
-                         CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES customer (id)
+CREATE TABLE "order"
+(
+    id          BIGSERIAL PRIMARY KEY,
+    description VARCHAR(255) NOT NULL,
+    customer_id BIGINT       NOT NULL,
+    CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES customer (id)
+);
+
+-- Create product table
+CREATE TABLE product
+(
+    id          BIGSERIAL PRIMARY KEY,
+    description VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE order_product
+(
+    order_id BIGINT       NOT NULL,
+    product_id BIGINT       NOT NULL,
+    CONSTRAINT fk_order_id FOREIGN KEY (order_id) REFERENCES "order" (id),
+    CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES product (id)
 );
