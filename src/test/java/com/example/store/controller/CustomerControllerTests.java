656d78dbb752ffbe5db8999d13b5f11f69a9f0f9
Farai Zihove <farai.zihove@econet.co.zw>
2025-02-19 16:19:33 +0200

    update order and customer tests to call processor

--- src/test/java/com/example/store/controller/CustomerControllerTests.java
+++ src/test/java/com/example/store/controller/CustomerControllerTests.java
@@ -1,27 +1,35 @@
 package com.example.store.controller;
 
+import com.example.store.dto.CustomerDTO;
 import com.example.store.entity.Customer;
 import com.example.store.mapper.CustomerMapper;
-import com.example.store.repository.CustomerRepository;
+import com.example.store.processor.api.CustomerProcessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CustomerController.class)
 @ComponentScan(basePackageClasses = CustomerMapper.class)
+@ExtendWith(SpringExtension.class)
 class CustomerControllerTests {
 
     @Autowired
@@ -31,35 +39,49 @@ class CustomerControllerTests {
     private ObjectMapper objectMapper;
 
     @MockitoBean
-    private CustomerRepository customerRepository;
+    private CustomerProcessor customerProcessor;
 
     private Customer customer;
+    private CustomerDTO customerDTO;
 
     @BeforeEach
     void setUp() {
         customer = new Customer();
         customer.setName("John Doe");
         customer.setId(1L);
+
+        customerDTO = new CustomerDTO();
+        customerDTO.setName("John Doe");
+        customerDTO.setId(1L);
     }
 
     @Test
     void testCreateCustomer() throws Exception {
-        when(customerRepository.save(customer)).thenReturn(customer);
+        when(customerProcessor.create(any())).thenReturn(customerDTO);
 
-        mockMvc.perform(post("/customer")
+        mockMvc.perform(MockMvcRequestBuilders.post("/customer")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(customer)))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.name").value("John Doe"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(customerDTO.getName()));
     }
 
     @Test
     void testGetAllCustomers() throws Exception {
-        when(customerRepository.findAll()).thenReturn(List.of(customer));
+        when(customerProcessor.findAll()).thenReturn(List.of(customerDTO));
 
         mockMvc.perform(get("/customer"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$..name").value("John Doe"));
-        ;
+                .andExpect(jsonPath("$..name").value(customerDTO.getName()));
+    }
+
+    @Test
+    void getAllCustomersMatchingSearchQuery() throws Exception {
+        when(customerProcessor.findCustomersMatchingSearchQuery(anyString())).thenReturn(List.of(customerDTO));
+
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/customer/search?name=john"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$..name").value(customerDTO.getName()));
     }
 }
--- src/test/java/com/example/store/controller/OrderControllerTests.java
+++ src/test/java/com/example/store/controller/OrderControllerTests.java
@@ -1,10 +1,12 @@
 package com.example.store.controller;
 
+import com.example.store.dto.OrderCustomerDTO;
+import com.example.store.dto.OrderDTO;
 import com.example.store.entity.Customer;
 import com.example.store.entity.Order;
 import com.example.store.mapper.CustomerMapper;
+import com.example.store.processor.api.OrderProcessor;
 import com.example.store.repository.CustomerRepository;
-import com.example.store.repository.OrderRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -19,8 +21,8 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
-import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -37,13 +39,15 @@ class OrderControllerTests {
     private ObjectMapper objectMapper;
 
     @MockitoBean
-    private OrderRepository orderRepository;
+    private OrderProcessor orderProcessor;
 
     @MockitoBean
     private CustomerRepository customerRepository;
 
     private Order order;
+    private OrderDTO orderDTO;
     private Customer customer;
+    private OrderCustomerDTO orderCustomerDTO;
 
     @BeforeEach
     void setUp() {
@@ -51,16 +55,25 @@ class OrderControllerTests {
         customer.setName("John Doe");
         customer.setId(1L);
 
+        orderCustomerDTO = new OrderCustomerDTO();
+        orderCustomerDTO.setName("John Doe");
+        orderCustomerDTO.setId(1L);
+
         order = new Order();
         order.setDescription("Test Order");
         order.setId(1L);
         order.setCustomer(customer);
+
+        orderDTO = new OrderDTO();
+        orderDTO.setDescription("Test Order");
+        orderDTO.setId(1L);
+        orderDTO.setCustomer(orderCustomerDTO);
     }
 
     @Test
     void testCreateOrder() throws Exception {
-        when(customerRepository.findById(1L)).thenReturn(Optional.of(customer));
-        when(orderRepository.save(order)).thenReturn(order);
+        when(customerRepository.save(customer)).thenReturn(customer);
+        when(orderProcessor.create(any())).thenReturn(orderDTO);
 
         mockMvc.perform(post("/order")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -72,7 +85,7 @@ class OrderControllerTests {
 
     @Test
     void testGetOrder() throws Exception {
-        when(orderRepository.findAll()).thenReturn(List.of(order));
+        when(orderProcessor.findAll()).thenReturn(List.of(orderDTO));
 
         mockMvc.perform(get("/order"))
                 .andExpect(status().isOk())
